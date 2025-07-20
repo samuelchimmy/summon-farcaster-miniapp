54efbaa34f0b5cd42d48779f9481dd1cf43b0e1b
xerxescoder <xerxescode@gmail.com>
2025-07-20 18:39:37 +0330

    fix redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/.well-known/farcaster.json",
+        destination: "https://api.farcaster.xyz/miniapps/hosted-manifest/0198285c-57c5-ad62-ecf6-545a3079e9d5",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
